c0593b7ad9f5946130cb4e6588e3f160268d3e61
velossoraptor <taliamolsen@gmail.com>
2025-09-04 09:57:14 -0400

    Fixed tests and Hello Message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router(); // Create a router instance
 // ^ was typing express().Router() which is incorrect, should be express.Router() (no paren after express)
 
 router.get("/", (req, res) => { // Default Hello World so that we're friendly :)
-    res.send("Hello World!");
+    res.send("Hello World! Type /contacts to see contact routes.");
 });
 
 router.use("/contacts", require("./contacts")); // Routes all /users requests to contacts.js
