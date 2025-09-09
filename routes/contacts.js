f0f8bdeb81c4d3e5ce88e6e4e941b8d4e529fa65
velossoraptor <taliamolsen@gmail.com>
2025-09-09 10:42:29 -0400

    Set up eslint and prettier (I think)

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router(); // Create a router instance
 // ^ was typing express().Router() which is incorrect, should be express.Router() (no paren after express)
 
-const contactsController = require("../controllers/contacts"); // Import the contacts controller
+const contactsController = require('../controllers/contacts'); // Import the contacts controller
 
-router.get("/", contactsController.getAll); // Routes all /users requests to contacts.js getAll function
+router.get('/', contactsController.getAll); // Routes all /users requests to contacts.js getAll function
 
-router.get("/:id", contactsController.getSingle); // Routes all /users/:id requests to contacts.js getSingle function
+router.get('/:id', contactsController.getSingle); // Routes all /users/:id requests to contacts.js getSingle function
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router(); // Create a router instance
+const a = 5;
 // ^ was typing express().Router() which is incorrect, should be express.Router() (no paren after express)
 
-router.get("/", (req, res) => { // Default Hello World so that we're friendly :)
-    res.send("Hello World! Type /contacts to see contact routes.");
+router.get('/', (req, res) => {
+  // Default Hello World so that we're friendly :)
+  res.send('Hello World! Type /contacts to see contact routes.');
 });
 
-router.use("/contacts", require("./contacts")); // Routes all /users requests to contacts.js
+router.use('/contacts', require('./contacts')); // Routes all /users requests to contacts.js
 
 module.exports = router;
