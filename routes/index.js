cf3ed813de5adb9482c83b69c45cb642b7cab850
velossoraptor <taliamolsen@gmail.com>
2025-09-09 13:23:25 -0400

    Swagger Express UI ~ customizing header

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router(); // Create a router instance
-const a = 5;
 // ^ was typing express().Router() which is incorrect, should be express.Router() (no paren after express)
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+var options = {
+  customCss: '.swagger-ui .topbar { display: none }'
+};
 
 router.get('/', (req, res) => {
   // Default Hello World so that we're friendly :)
@@ -9,5 +14,8 @@ router.get('/', (req, res) => {
 });
 
 router.use('/contacts', require('./contacts')); // Routes all /users requests to contacts.js
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongodb = require('./db/connect');
 
+
 // ITS WORKINGGGGG!!!!
 // YIPPEE YIPPEE
 
